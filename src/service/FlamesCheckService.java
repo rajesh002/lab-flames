33554efa72dabe926dcb6a6b0e2c3d89201a7a89
rajesh002 <rajeshbtech16@gmail.com>
2020-03-25 02:05:20 +0530

    Flames project suceesfully finished

--- src/service/FlamesCheckService.java
+++ src/service/FlamesCheckService.java
@@ -7,3 +7,52 @@ package service;
 // You must return only the following values ['f','l','a','m','e','s']
 // change the return value at the end of the method corresponding to your return value
 
+ public class FlamesCheckService{
+	 public char findFlames(String name1,String name2) {
+		StringBuilder str = new StringBuilder(name2);
+		char temp,temp1;
+		int name1Size=name1.length();
+		int name2Size=name2.length();
+		int totalSize=name1Size+name2Size;
+	 
+	    for (int index=0; index<name1Size; index++){
+	           for (int index1=0; index1<name2Size; index1++){
+	        	   temp=name1.charAt(index);
+	        	   temp1=str.charAt(index1);
+	                 if(temp==temp1) {
+	                	 totalSize-=2;
+	                	 str.setCharAt(index1, '0');
+	                 }
+	          }
+	    }
+	    return checkFlames(totalSize);
+	}
+	char checkFlames(int size) {
+		int start=0,indexCount,flameCount=0;
+	    String tmp;
+	    String a;
+	    StringBuilder flames = new StringBuilder("flames");
+	     while(start<flames.length()) {
+	         tmp=Character.toString(flames.charAt(start));
+	    	indexCount=flames.indexOf(tmp)+1;
+	    	flameCount+=1;
+	    	if(flameCount==size){
+	    	     flames.deleteCharAt(indexCount-1);
+	    	     start--;
+	    	    if(flames.length()==1)
+	    	    break;
+	    	    else
+	    	    flameCount=0;
+	    	}
+	    	//System.out.print(m);
+	    	start++;
+	    	if(start==flames.length())
+	    	start=0;
+	    }
+	    
+	    return flames.charAt(0);
+	}
+	 
+	    	
+	    }
+	
